af650e38028339ac584a917f692ac5b9b3c9265c
Dule2410 <56434110+Dule2410@users.noreply.github.com>
2019-12-06 12:51:00 +0100

    Add files via upload

---
+++ popisUc.php
@@ -0,0 +1,5 @@
+<?php
+$popisUC = array("bilic.tereza", "cepo.cvitan.luka", "frankovic.rea", "gale.antonela", "horvat.tomislav", "ilijevi.josip", "kerim.alberto", "kosovic.bartol",
+    "kupresak.marko", "matic.jan", "nogic.nino", "pavlekovic.dino", "rodic.tin", "rumiha.petar", "savic.luka", "stracenski.monika", "saric.matej", "skrlec.luka",
+    "splajt.renato", "vlatkovic.josip", "vukovic.patrik", "zaninovic.ana");
+
