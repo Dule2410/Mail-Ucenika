e98b69a10eaf96e6ea8cc60bdebd716e5eb07d98
Dule2410 <56434110+Dule2410@users.noreply.github.com>
2019-12-09 14:26:17 +0100

    Update popisUc.php

--- popisUc.php
+++ popisUc.php
@@ -1,6 +1,10 @@
 <?php
-$popisUC = array("bilic.tereza", "cepo.cvitan.luka", "frankovic.rea", "gale.antonela", "horvat.tomislav", "ilijevi.josip", "kerim.alberto", "kosovic.bartol",
+$popisUC = array("bilic.tereza", "cepocvitan.luka", "frankovic.rea", "gale.antonela", "horvat.tomislav", "ilijevi.josip", "kerim.alberto", "kosovic.bartol",
     "kupresak.marko", "matic.jan", "nogic.nino", "pavlekovic.dino", "rodic.tin", "rumiha.petar", "savic.luka", "stracenski.monika", "saric.matej", "skrlec.luka",
     "splajt.renato", "vlatkovic.josip", "vukovic.patrik", "zaninovic.ana");
 
+$regex = "@gmail.com";
 $user_name = filter_input(INPUT_GET, 'user_name');
+if (in_array($user_name, $popisUC)) {
+    echo $user_name , $regex;
+}
