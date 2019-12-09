d3626072bf2fd4e40d051827dc5f5e6699d14049
Dule2410 <56434110+Dule2410@users.noreply.github.com>
2019-12-09 13:46:24 +0100

    Update popisUc.php

--- popisUc.php
+++ popisUc.php
@@ -3,3 +3,4 @@ $popisUC = array("bilic.tereza", "cepo.cvitan.luka", "frankovic.rea", "gale.anto
     "kupresak.marko", "matic.jan", "nogic.nino", "pavlekovic.dino", "rodic.tin", "rumiha.petar", "savic.luka", "stracenski.monika", "saric.matej", "skrlec.luka",
     "splajt.renato", "vlatkovic.josip", "vukovic.patrik", "zaninovic.ana");
 
+$user_name = filter_input(INPUT_GET, 'user_name');
